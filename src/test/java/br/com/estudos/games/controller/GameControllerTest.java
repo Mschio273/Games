b1ec0c23064b693dbc3719990abe9d46e882113f
Matheus Schio <matheus.schiod@gmail.com>
2021-08-19 12:02:18 -0300

    removendo imports desnecessarios

--- src/test/java/br/com/estudos/games/controller/GameControllerTest.java
+++ src/test/java/br/com/estudos/games/controller/GameControllerTest.java
@@ -2,25 +2,16 @@ package br.com.estudos.games.controller;
 
 import br.com.estudos.games.model.Game;
 import br.com.estudos.games.repository.GameRepository;
-import br.com.estudos.games.service.GameService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //@SpringBootTest
 @ExtendWith(MockitoExtension.class)
--- src/test/java/br/com/estudos/games/service/GameServiceTest.java
+++ src/test/java/br/com/estudos/games/service/GameServiceTest.java
@@ -2,15 +2,11 @@ package br.com.estudos.games.service;
 
 import br.com.estudos.games.model.Game;
 import br.com.estudos.games.repository.GameRepository;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
